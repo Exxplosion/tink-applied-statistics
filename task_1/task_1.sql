9526cd944106d7c44cb3082d09abf71be78719f2
Exxplosion <nikitosfly200093@gmail.com>
2021-10-08 08:39:50 +0300

    add first task

---
+++ task_1/task_1.sql
@@ -0,0 +1,33 @@
+--Сколько уникальных моделей самолетов?
+select sum(count_unique_models) from (select count(model) as count_unique_models
+	from bookings.aircrafts
+	group by model) a;
+
+--Найдите топ 5 самых популярных городов. Ответ в порядке убывания.
+with cte as (
+	select flights.flight_id,
+	airports.city,
+	ticket_flights.ticket_no,
+	ticket_flights.amount
+	from bookings.flights
+	inner join bookings.airports on airports.airport_code = flights.arrival_airport
+	inner join bookings.ticket_flights on flights.flight_id = ticket_flights.flight_id
+)
+
+select distinct city, count(ticket_no) over(partition by city) as popular_city from cte order by popular_city DESC;
+--Комментарий: в какой метрике считать "популярность" города? Я выбрал количество купленных билетов для перелета в город.
+
+--Какая сумма всех потраченных денег на перелет из Москвы?
+with cte_1 as (
+	select flights.flight_id,
+	airports.city,
+	ticket_flights.ticket_no,
+	ticket_flights.amount
+	from bookings.flights
+	inner join bookings.airports on airports.airport_code = flights.departure_airport
+	inner join bookings.ticket_flights on flights.flight_id = ticket_flights.flight_id
+)
+
+select distinct city, sum(amount) over(partition by city) as amount_city from cte_1 where city like 'Москва';
+
+--Задание 4 в след. файле.
