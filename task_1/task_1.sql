d225e5f61a8764c5db80d277cf62789d758b49e4
Exxplosion <55700851+Exxplosion@users.noreply.github.com>
2021-10-11 16:03:23 +0300

    Update task_1.sql

--- task_1/task_1.sql
+++ task_1/task_1.sql
@@ -2,6 +2,7 @@
 select sum(count_unique_models) from (select count(model) as count_unique_models
 	from bookings.aircrafts
 	group by model) a;
+--Ответ: 9
 
 --Найдите топ 5 самых популярных городов. Ответ в порядке убывания.
 with cte as (
@@ -16,6 +17,8 @@ with cte as (
 
 select distinct city, count(ticket_no) over(partition by city) as popular_city from cte order by popular_city DESC;
 --Комментарий: в какой метрике считать "популярность" города? Я выбрал количество купленных билетов для перелета в город.
+--Ответ: Москва, Санкт-Петербург, Новосибирск, Екатиренбург, Сочи
+
 
 --Какая сумма всех потраченных денег на перелет из Москвы?
 with cte_1 as (
@@ -29,5 +32,6 @@ with cte_1 as (
 )
 
 select distinct city, sum(amount) over(partition by city) as amount_city from cte_1 where city like 'Москва';
+--Ответ: 7462943300
 
 --Задание 4 в след. файле.
